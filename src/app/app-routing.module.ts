40a8a6a545016e7d54e34582d70add28d243b7a5
DaltonJComer <dalton.comer@outlook.com>
2022-03-19 20:09:07 -0400

    minor styling for buy and rent pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RentComponent } from './rent/rent.component';
+import { BuyComponent } from './buy/buy.component';
+import { SellComponent } from './sell/sell.component';
+import { LearnComponent } from './learn/learn.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'rent',component: RentComponent},
+  {path: 'buy',component: BuyComponent},
+  {path: 'sell',component: SellComponent},
+  {path: 'learn',component: LearnComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RentComponent } from './rent/rent.component';
+import { BuyComponent } from './buy/buy.component';
+import { SellComponent } from './sell/sell.component';
+import { LearnComponent } from './learn/learn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RentComponent,
+    BuyComponent,
+    SellComponent,
+    LearnComponent
   ],
   imports: [
     BrowserModule,
