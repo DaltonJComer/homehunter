834bbf19a1f2aae75a33c0266021edab85b6ca6b
DaltonJComer <dalton.comer@outlook.com>
2022-03-21 17:30:48 -0400

    Added listing submission and listing viewing

--- src/app/buy/buy.component.ts
+++ src/app/buy/buy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-buy',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyComponent implements OnInit {
 
-  constructor() { }
+  data: any = [];
+
+  constructor(private db: AngularFireDatabase) { }
 
   ngOnInit(): void {
+    const ref = this.db.list("Listings");
+    ref.valueChanges().subscribe((data)=>{
+    this.data = data;
+    console.log(data);
+  })
   }
 
 }
