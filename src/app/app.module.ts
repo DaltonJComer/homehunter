ae8d5a26d07df6127841be586b7ee7991aa75c4f
MazBam <33433905+MazinBamohrez@users.noreply.github.com>
2022-03-20 22:14:16 -0400

    database integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+
+
+
+
+
+export const firebaseConfig={
+  apiKey: "AIzaSyBfmSrOUgl2EiP-zSL3zpvyRxyPkJqBHqo",
+  authDomain: "homehunter-cosc3p94.firebaseapp.com",
+  databaseURL: "https://homehunter-cosc3p94-default-rtdb.firebaseio.com",
+  projectId: "homehunter-cosc3p94",
+  storageBucket: "homehunter-cosc3p94.appspot.com",
+  messagingSenderId: "713223580883",
+  appId: "1:713223580883:web:52fb46fd3530c01989b826",
+  measurementId: "G-E56XXYXNGG"
+}
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RentComponent } from './rent/rent.component';
@@ -8,6 +28,7 @@ import { BuyComponent } from './buy/buy.component';
 import { SellComponent } from './sell/sell.component';
 import { LearnComponent } from './learn/learn.component';
 import { HomeComponent } from './home/home.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,6 +41,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase), // initializes the database
+    AngularFireDatabaseModule, 
     AppRoutingModule
   ],
   providers: [],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
-export const environment = {
+export const environment = { // might not need this but i added the config information in both environment ts files just in case 
+  firebase: {
+    projectId: 'homehunter-cosc3p94',
+    appId: '1:713223580883:web:52fb46fd3530c01989b826',
+    databaseURL: 'https://homehunter-cosc3p94-default-rtdb.firebaseio.com',
+    storageBucket: 'homehunter-cosc3p94.appspot.com',
+    apiKey: 'AIzaSyBfmSrOUgl2EiP-zSL3zpvyRxyPkJqBHqo',
+    authDomain: 'homehunter-cosc3p94.firebaseapp.com',
+    messagingSenderId: '713223580883',
+    measurementId: 'G-E56XXYXNGG',
+  },
   production: true
 };
